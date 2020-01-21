702b19a488e1cfb20d34ae8096c301f27fc555b6
Stefan Fussenegger <stf+git@molindo.at>
2020-01-21 12:11:30 +0100

    add docker image for docker-only builds

---
+++ docker/bin/dockerBuild.sh
@@ -0,0 +1,57 @@
+#!/bin/bash -e
+#
+# build Dockerfile and push to ECR registry
+#
+
+# pipelines env
+slug=${BITBUCKET_REPO_SLUG?}
+commit=${BITBUCKET_COMMIT?}
+branch=${BITBUCKET_BRANCH}
+
+# required variables
+registry=${MOLINDO_DOCKER_REGISTRY?}
+bucket=${MOLINDO_DOCKER_BUCKET?}
+
+# force env variables used by awscli
+key=${AWS_ACCESS_KEY_ID?}
+secret=${AWS_SECRET_ACCESS_KEY?}
+
+# optional variables
+tags=${CONTAINER_TAGS:-container-tags.txt}
+
+# optional args
+path=${1:-.}
+
+target=$slug:$commit
+
+if [ -e $tags ]; then
+	echo "using existing $tags"
+else
+	echo "generating default $tags"
+	echo -n $target > $tags
+fi
+
+# ECR login
+if [ `echo $registry | grep 'ecr\.[^.]*\.amazonaws\.com$'` ]; then
+	region=`echo $registry | sed -e 's/^.*\.\([^.]*\)\.amazonaws\.com$/\1/g'`
+	echo "logging in to AWS ECR in $region"
+	$( aws --region $region ecr get-login --no-include-email )
+fi
+
+echo "building $target"
+docker build -t $target $path
+
+cat $tags
+
+cat $tags | while read tag || [ -n "$tag" ]; do
+	if [ -n "$tag" ]; then
+		echo "pushing $registry/$tag"
+		docker tag $target $registry/$tag
+		docker push $registry/$tag
+	fi
+done
+
+if [ -n "$branch" ]; then
+	echo "uploading $tags to $bucket as artifacts/${branch}/${slug}-container-tags.txt"
+	aws s3api put-object --bucket $bucket --key artifacts/${branch}/${slug}-container-tags.txt --body ${tags}
+fi
---
+++ docker/bin/triggerBamboo.sh
@@ -0,0 +1,56 @@
+#!/bin/bash -e
+#
+# trigger bamboo build for same commit
+#
+
+if [ -z "${BITBUCKET_BRANCH}" ]; then
+        echo "not a branch build, exiting"
+        exit 0
+fi
+
+export out=$( mktemp --tmpdir trigger.XXXXXX )
+function errorTrap {
+    echo "last output:"
+    cat $out
+    exit 255
+}
+trap errorTrap ERR
+
+# pipelines env
+branch=${BITBUCKET_BRANCH?}
+commit=${BITBUCKET_COMMIT?}
+
+# required variables
+bamboo=${BAMBOO_ROOT?}
+plan=${BAMBOO_PLAN:-${BAMBOO_CHILD_PLAN?}}
+child=${BAMBOO_CHILD_PLAN}
+user=${BAMBOO_USER?}
+pass=${BAMBOO_PASS?}
+
+echo "triggering build for ${plan}"
+if [ "${branch}" = "master" ]; then
+    branchKey=${plan}
+else
+    echo "getting branch key for ${branch}"
+    branchKey=$( curl -s --user "${user}:${pass}" -H 'Accept: application/json' ${bamboo}/rest/api/latest/plan/${plan}/branch/${branch} | tee $out | jshon -e key -u 2> /dev/null || echo "" )
+
+    if [ -z "${branchKey}" ]; then
+        if [ "${plan}" = "${child}" ]; then
+            echo "not triggering unknown branch in child plan ${plan}"
+            exit
+        else
+            echo "creating new branch ${branch} of plan ${plan}"
+            branchKey=$( curl -s --user "${user}:${pass}" -H 'Accept: application/json' -XPUT "${bamboo}/rest/api/latest/plan/${plan}/branch/${branch}?vcsBranch=${branch}&enabled=true&cleanupEnabled=true" | tee $out | jshon -e key -u )
+        fi
+    fi
+fi
+
+if [ "${plan}" = "${child}" ]; then
+    echo "triggering build in ${branchKey} with user ${user}"
+    resultKey=$(curl -s --user "${user}:${pass}" -H 'Accept: application/json' -XPOST ${bamboo}/rest/api/latest/queue/${branchKey} | tee $out | jshon -e buildResultKey -u)
+else
+    echo "triggering build for ${commit} in ${branchKey} with user ${user}"
+    resultKey=$(curl -s --user "${user}:${pass}" -H 'Accept: application/json' -XPOST ${bamboo}/rest/api/latest/queue/${branchKey}?customRevision=${commit} | tee $out | jshon -e buildResultKey -u)
+fi
+
+echo "queued build ${buildResultKey}"
